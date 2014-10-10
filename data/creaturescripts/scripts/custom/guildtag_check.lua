9d0ddee1118b124621ed9f7b98dc415f01be9357
Timothy Engel <engeltj@gmail.com>
2014-10-10 05:42:27 +0000

    Bug fixes

--- data/creaturescripts/scripts/custom/guildtag_check.lua
+++ data/creaturescripts/scripts/custom/guildtag_check.lua
@@ -1,9 +1,9 @@
 function onLogout(cid)
 	local name = getPlayerName(cid)
-	local tag = getGuildTag(getPlayerGuildId(cid))
+	local tag = getGuildTag(getPlayerGuildId(cid)) or ""
 
 	if(string.len(tag) == 0) then
-		local newname = string.gsub(name, '(%[%w+%]%s)', "")
+		local newname = string.gsub(name, '(%[%w+%])', "")
 		doPlayerChangeName(getPlayerName(cid), newname)
 	else
 		local newname = string.gsub(name, '(%[%w+%])', "%["..tag.."%]")
